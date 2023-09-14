346a877af9c8eac20c8f12e17998b5f1554e271b
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2023-09-14 10:46:34 +0600

    Add files via upload

---
+++ Armstrong Number between 1 to N.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,sum,i,temp,a;
+    printf("The Armstrong numbers in between 1 to 500 are :");
+    for(i = 1; i <= 500; i++)
+    {
+        temp = i;
+        sum = 0;
+        while(temp != 0)
+        {
+            a = temp%10;
+            sum += a*a*a;
+            temp = temp/10;
+        }
+        if(sum == i)
+        printf("\n\t\t\t%d", i);
+    }
+    return 0;
+}
